ca9dd07728a07174ccd91e74452b1027f67356e7
Alexey Martemyanov <mallexxx@gmail.com>
2025-04-14 20:19:35 +0600

    Lottie -> 4.5.1 (#481)
    <!--
    Note: This template is a reminder of our Engineering Expectations and
    Definition of Done. Remove sections that don't apply to your changes.
    
    ⚠️ If you're an external contributor, please file an issue before
    working on a PR. Discussing your changes beforehand will help ensure
    they align with our roadmap and that your time is well spent.
    -->
    
    Task/Issue URL:
    https://app.asana.com/1/137249556945/project/1201037661562251/task/1209958084516053
    
    ### Description
    
    ### Testing Steps
    <!-- Assume the reviewer is unfamiliar with this part of the app -->
    1. Validate CI checks pass
    2. Launch macOS app, validate the Fire button and Fire animations work
    3. Launch iOS app, validate Fire animation works
    
    ### Impact and Risks
    <!-- 
    What's the impact on users if something goes wrong?
    
    High: Could affect user privacy, lose user data, break core
    functionality
    Medium: Could disrupt specific features or user flows
    Low: Minor visual changes, small bug fixes, improvement to existing
    features
    None: Internal tooling, documentation
    -->
    
    #### What could go wrong?
    <!-- Describe specific scenarios and how you've addressed them -->
    
    ### Quality Considerations
    <!-- 
    Focus on what matters for your changes:
    - What edge cases exist?
    - How does this affect performance?
    - What monitoring have you added?
    - What documentation needs updating?
    - How does this impact privacy/security?
    -->
    
    ### Notes to Reviewer
    <!-- Anything specific you want reviewers to focus on -->
    
    ---
    ###### Internal references:
    [Definition of
    Done](https://app.asana.com/0/1202500774821704/1207634633537039/f) |
    [Engineering
    Expectations](https://app.asana.com/0/59792373528535/199064865822552) |
    [Tech Design
    Template](https://app.asana.com/0/59792373528535/184709971311943)

--- macOS/LocalPackages/NetworkProtectionMac/Package.swift
+++ macOS/LocalPackages/NetworkProtectionMac/Package.swift
@@ -35,7 +35,7 @@ let package = Package(
         .library(name: "VPNExtensionManagement", targets: ["VPNExtensionManagement"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/airbnb/lottie-spm", exact: "4.4.3"),
+        .package(url: "https://github.com/airbnb/lottie-spm", exact: "4.5.1"),
         .package(path: "../../SharedPackages/BrowserServicesKit"),
         .package(path: "../AppInfoRetriever"),
         .package(path: "../AppLauncher"),
