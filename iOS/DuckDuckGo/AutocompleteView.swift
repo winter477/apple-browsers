53a2964f25227dafa7914986528b8f46f91a4010
Mariusz Śpiewak <dus7@users.noreply.github.com>
2025-06-04 16:26:58 +0200

    Fix favorites E2E test (#945)
    <!--
    Note: This template is a reminder of our Engineering Expectations and
    Definition of Done. Remove sections that don't apply to your changes.
    
    ⚠️ If you're an external contributor, please file an issue before
    working on a PR. Discussing your changes beforehand will help ensure
    they align with our roadmap and that your time is well spent.
    -->
    
    Task/Issue URL:
    https://app.asana.com/1/137249556945/project/1206226850447395/task/1210447750549951?focus=true
    Tech Design URL:
    CC:
    
    ### Description
    
    Adds identifiers for autocomplete list and uses it instead of icon name
    in the test.
    
    ### Testing Steps
    1. Make sure favorite E2E test pass
    
    ### Impact and Risks
    None: Internal tooling, documentation
    
    ---
    ###### Internal references:
    [Definition of
    Done](https://app.asana.com/0/1202500774821704/1207634633537039/f) |
    [Engineering
    Expectations](https://app.asana.com/0/59792373528535/199064865822552) |
    [Tech Design
    Template](https://app.asana.com/0/59792373528535/184709971311943)

--- iOS/DuckDuckGo/AutocompleteView.swift
+++ iOS/DuckDuckGo/AutocompleteView.swift
@@ -234,36 +234,42 @@ private struct SuggestionView: View {
                                    query: query,
                                    indicator: tapAheadImage) {
                     autocompleteModel.onTapAhead(model)
-                }
+                }.accessibilityIdentifier("Autocomplete.Suggestions.ListItem.SearchPhrase-\(phrase)")
 
             case .website(let url):
                 SuggestionListItem(icon: Image(uiImage: DesignSystemImages.Glyphs.Size24.globe),
                                    title: url.formattedForSuggestion())
+                .accessibilityIdentifier("Autocomplete.Suggestions.ListItem.Website-\(url.formattedForSuggestion())")
 
             case .bookmark(let title, let url, let isFavorite, _) where isFavorite:
                 SuggestionListItem(icon: Image(uiImage: DesignSystemImages.Glyphs.Size24.bookmarkFavorite),
                                    title: title,
                                    subtitle: url.formattedForSuggestion())
+                .accessibilityIdentifier("Autocomplete.Suggestions.ListItem.Favorite-\(url.formattedForSuggestion())")
 
             case .bookmark(let title, let url, _, _):
                 SuggestionListItem(icon: Image(uiImage: DesignSystemImages.Glyphs.Size24.bookmark),
                                    title: title,
                                    subtitle: url.formattedForSuggestion())
+                .accessibilityIdentifier("Autocomplete.Suggestions.ListItem.Bookmark-\(url.formattedForSuggestion())")
 
             case .historyEntry(_, let url, _) where url.isDuckDuckGoSearch:
                 SuggestionListItem(icon: Image(uiImage: DesignSystemImages.Glyphs.Size24.history),
                                    title: url.searchQuery ?? "",
                                    subtitle: UserText.autocompleteSearchDuckDuckGo)
+                .accessibilityIdentifier("Autocomplete.Suggestions.ListItem.SERPHistory-\(url.searchQuery ?? "")")
 
             case .historyEntry(let title, let url, _):
                 SuggestionListItem(icon: Image(uiImage: DesignSystemImages.Glyphs.Size24.history),
                                    title: title ?? "",
                                    subtitle: url.formattedForSuggestion())
+                .accessibilityIdentifier("Autocomplete.Suggestions.ListItem.History-\(url.formattedForSuggestion())")
 
             case .openTab(title: let title, url: let url, _, _):
                 SuggestionListItem(icon: Image(uiImage: DesignSystemImages.Glyphs.Size24.tabMobile),
                                    title: title,
                                    subtitle: "\(UserText.autocompleteSwitchToTab) · \(url.formattedForSuggestion())")
+                .accessibilityIdentifier("Autocomplete.Suggestions.ListItem.OpenTab-\(url.formattedForSuggestion())")
 
             case .internalPage, .unknown:
                 FailedAssertionView("Unknown or unsupported suggestion type")
