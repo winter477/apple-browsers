fdccebfbdd2ddcfe0636d3c38dba09363054561b
Michal Smaga <miasma13@gmail.com>
2025-07-18 11:26:22 +0200

    Disable flaky testCosmeticRule test (#1416)
    <!--
    Note: This template is a reminder of our Engineering Expectations and
    Definition of Done. Remove sections that don't apply to your changes.
    
    ⚠️ If you're an external contributor, please file an issue before
    working on a PR. Discussing your changes beforehand will help ensure
    they align with our roadmap and that your time is well spent.
    -->
    
    Task/Issue URL:
    https://app.asana.com/1/137249556945/project/1205237866452338/task/1210788044594391?focus=true
    Tech Design URL:
    CC:
    
    ### Description
    Disable flaky `testCosmeticRule` test
    
    ### Testing Steps
    Ensure tests are green.
    
    ### Impact and Risks
    None: Internal tooling, documentation
    
    ---
    ###### Internal references:
    [Definition of
    Done](https://app.asana.com/0/1202500774821704/1207634633537039/f) |
    [Engineering
    Expectations](https://app.asana.com/0/59792373528535/199064865822552) |
    [Tech Design
    Template](https://app.asana.com/0/59792373528535/184709971311943)

--- iOS/IntegrationTests/AutoconsentBackgroundTests.swift
+++ iOS/IntegrationTests/AutoconsentBackgroundTests.swift
@@ -109,7 +109,9 @@ final class AutoconsentBackgroundTests: XCTestCase {
     }
 
     @MainActor
-    func testCosmeticRule() {
+    func testCosmeticRule() throws {
+        throw XCTSkip("Flaky test")
+
         let configuration = WKWebViewConfiguration()
 
         configuration.userContentController.addUserScript(autoconsentUserScript.makeWKUserScriptSync())
