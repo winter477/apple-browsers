1095e2579f1933e1169264588baf6afe71ea0f92
Michal Smaga <miasma13@gmail.com>
2025-08-06 18:15:40 +0200

    Global AI Switch 3. Settings copy update and visual tweaks (#1600)
    Task/Issue URL:
    https://app.asana.com/1/137249556945/project/72649045549333/task/1210702047347360?focus=true
    Tech Design URL:
    CC:
    
    ### Description
    Settings copy update, their translation and visual tweaks. Feature flag
    backed up by privacy config `aiChat`'s `globalToggle` subfeature.
    
    ### Testing Steps
    1. Ensure that feature is hidden behind feature flag
    2. Verify settings design is correct with
    [Figma](https://www.figma.com/design/JXYJ3Gweytvrvfliops6T3/Duck.ai-Settings-Integration?node-id=0-1&t=tQPbT57XtJOwRbo6-1)
    3. Verify that "AI Features" settings pane is correctly translated
    
    ### Impact and Risks
    Low: Minor visual changes, small bug fixes, improvement to existing
    features
    
    #### What could go wrong?
    Low: Minor visual changes, small bug fixes, improvement to existing
    features
    
    
    ---
    ###### Internal references:
    [Definition of
    Done](https://app.asana.com/0/1202500774821704/1207634633537039/f) |
    [Engineering
    Expectations](https://app.asana.com/0/59792373528535/199064865822552) |
    [Tech Design
    Template](https://app.asana.com/0/59792373528535/184709971311943)

--- macOS/LocalPackages/FeatureFlags/Sources/FeatureFlags/FeatureFlag.swift
+++ macOS/LocalPackages/FeatureFlags/Sources/FeatureFlags/FeatureFlag.swift
@@ -313,7 +313,7 @@ extension FeatureFlag: FeatureFlagDescribing {
         case .removeWWWInCanonicalizationInThreatProtection:
             return .remoteReleasable(.subfeature(MaliciousSiteProtectionSubfeature.removeWWWInCanonicalization))
         case .aiChatGlobalSwitch:
-            return .internalOnly()
+            return .remoteReleasable(.subfeature(AIChatSubfeature.globalToggle))
         case .aiChatSidebar:
             return .remoteReleasable(.subfeature(AIChatSubfeature.sidebar))
         case .aiChatTextSummarization:
